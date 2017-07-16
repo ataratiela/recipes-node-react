198c7f805a70d4165bd5144610c3d311b43f7b66
Sergio Garcia <srgarcia91@gmail.com>
2017-07-16 15:04:27 +0200

    Created the app server backend

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();;
+const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
+    res.status(200).send('Hello World!');
 });
 
 module.exports = app;

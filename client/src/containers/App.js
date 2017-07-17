12374f13b346725124dc9aec4b50c1f43d5a2af5
Sergio Garcia <srgarcia91@gmail.com>
2017-07-17 21:49:35 +0200

    Creating the initial folder structure for the React App

---
+++ client/src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello Recipes on React!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;

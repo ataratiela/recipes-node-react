5d5fffa3a7e5961c4d4e257c29ca360dbca71736
alejalonta@gmail.com <alejalonta@gmail.com>
2017-07-24 13:21:28 +0200

    WIP user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,51 @@ const router = express.Router();
 const user = require('../models/user');
 
 router.get('/', (req, res) => {
-    user.getAll((error, result) => {
+    user.findAll((error, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( error || result ));
+    });
+});
+
+router.get('/edit/:userID', (req, res) => {
+    const userID = req.params.userID;
+
+    user.findByID(userID, (error, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( error || result ));
+    });
+});
+
+router.get('/add', (req, res) => {
+    const user = {
+        userID : req.body.userID,
+		name : req.body.name,
+		password : req.body.password
+    }
+
+    user.save(user, (error, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( error || result ));
+    });
+});
+
+router.put('/update/:userID', (req, res) => {
+    const user = {
+        userID : req.body.userID,
+		name : req.body.name,
+		password : req.body.password
+    }
+
+    user.update(user, (error, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( error || result ));
+    });
+});
+
+router.delete('/delete/:userID', (req, res) => {
+    const userID = req.params.userID;
+
+    user.delete(userID, (error, result) => {
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify( error || result ));
     });

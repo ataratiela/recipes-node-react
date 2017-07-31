fee630bb55608f6512739564bbd909adc8b40fa0
Sergio Garcia <srgarcia91@gmail.com>
2017-07-31 19:30:58 +0200

    Add navbar to app

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import Navbar from '../views/Navbar';
 import Recipes from './Recipes';
 import Recipe from './Recipe';
 
@@ -11,6 +12,8 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Navbar />
+
           <Route exact path="/" component={ Recipes }/>
           <Route path="/recipe/:id" component={ Recipe }/>
         </div>

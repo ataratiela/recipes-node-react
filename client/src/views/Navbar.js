3fcfdf2f6171c4c4cb54723fc2694f0521d94cb7
Sergio Garcia <srgarcia91@gmail.com>
2017-07-31 19:30:44 +0200

    Create component Navbar

---
+++ client/src/views/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+function Navbar(props) {
+  const { location } = props
+
+  console.log(props);
+
+  return (
+    <div className='Navbar'>
+      <div className='nav-content full-width'>
+        <div className='nav-primary'>
+          <Link className='btn btn-nav' to='/'>Home</Link>
+        </div>
+        <div className='nav-secondary'>
+          <Link className='btn btn-fill' to='/'>Log Out</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

1b1bd7cad2acc8582730e9d82f1dfdd487a8edf5
Sergio Garcia <srgarcia91@gmail.com>
2017-08-07 19:58:59 +0200

    Added all the inputs but the recipe steps

--- client/src/containers/NewRecipe.js
+++ client/src/containers/NewRecipe.js
@@ -6,35 +6,51 @@ class NewRecipe extends Component {
     super(props);
 
     this.state = {
-      name: '',
-      description: '',
-      image: null,
-      difficulty: null,
-      diners: null,
-      prepTime: null,
-      category: null
+      recipe: {
+        name: '',
+        description: '',
+        image: '',
+        difficulty: null,
+        diners: null,
+        prepTime: null,
+        category: null
+      },
+      categories: []
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDifficultyChange = this.handleDifficultyChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(event.target);
+  }
+
+  handleDifficultyChange(event) {
+    const difficulty = event.target.name;
+
+    let recipe = Object.assign({}, this.state.recipe);
+
+    recipe.difficulty = Number(difficulty.split('-').pop());
+
+    this.setState({ recipe: recipe });
   }
 
   render() {
+    const recipe = this.state.recipe;
+
     return (
       <div className='content full-width'>
         <RecipeForm
-          name={ this.state.name }
-          description={ this.state.description }
-          image={ this.state.image }
-          difficulty={ this.state.difficulty }
-          diners={ this.state.diners }
-          prepTime={ this.state.prepTime }
-          category={ this.state.category }
-          submit={ this.handleSubmit } />
+          name={ recipe.name }
+          description={ recipe.description }
+          image={ recipe.image }
+          difficulty={ recipe.difficulty }
+          diners={ recipe.diners }
+          prepTime={ recipe.prepTime }
+          category={ recipe.category }
+          submit={ this.handleSubmit } 
+          handleDifficultyChange={ this.handleDifficultyChange } />
       </div>
     );
   }
--- client/src/views/RecipeForm.js
+++ client/src/views/RecipeForm.js
@@ -6,8 +6,56 @@ function RecipeForm (props) {
       <input
         name='name'
         type='text'
-        placeholder='Recipe Name'
+        placeholder='Recipe name...'
         value={ props.name } />
+      <textarea
+        name='description'
+        placeholder='Recipe description...'
+        value={ props.description } />
+      <label>
+        Recipe image:
+        <input
+          name='image'
+          type='file'
+          value={ props.image } />
+      </label>
+      <label>
+        Diners:
+        <input
+          name='diners'
+          type='number'
+          min='1'
+          step='1'
+          value={ props.image } />
+      </label>
+      <label>
+        Difficulty:
+        <input
+          name='difficulty-1'
+          type='checkbox'
+          checked={ props.difficulty >= 1 }
+          onChange={ props.handleDifficultyChange } />
+        <input
+          name='difficulty-2'
+          type='checkbox'
+          checked={ props.difficulty >= 2 }
+          onChange={ props.handleDifficultyChange } />
+        <input
+          name='difficulty-3'
+          type='checkbox'
+          checked={ props.difficulty >= 3 }
+          onChange={ props.handleDifficultyChange } />
+        <input
+          name='difficulty-4'
+          type='checkbox'
+          checked={ props.difficulty >= 4 }
+          onChange={ props.handleDifficultyChange } />
+        <input
+          name='difficulty-5'
+          type='checkbox'
+          checked={ props.difficulty >= 5 }
+          onChange={ props.handleDifficultyChange } />
+      </label>
       <input type="submit" value="Create Recipe" />
     </form>
   );

8f07c23e3ff9f2d5d15ab97ea6d93695665867a7
Sergio Garcia <srgarcia91@gmail.com>
2017-08-08 16:15:07 +0200

    Category model with findAll method

---
+++ models/category.js
@@ -0,0 +1,22 @@
+var dbPool = require('../db/db-connector');
+
+const recipe = ({ CategoyID, Name }) => {
+
+  let state = {
+    categoryID: CategoryID,
+    name: Name
+  }
+
+  return {
+    state
+  } 
+}
+
+exports.findAll = (done) => {
+  dbPool.query('SELECT * FROM Categories', (err, res, fields) => {
+    if (err) throw err;
+
+    let categories = res.map(r => category(r)).map(c => c.state);
+    done(null, categories);
+  });
+};

baae7f69995692fe4975e9ff29972364f52554ac
Sergio Garcia <srgarcia91@gmail.com>
2017-08-09 20:36:22 +0200

    Added the save method to the model, and the router call

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,42 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const recipe = require('../models/recipe');
-
-function formatImage(name, image) {
-    let fileName = name.toLowerCase().replace(' ', '-');
-    let fileData = null;
-
-    if(fileName.includes('.png')) {
-        fileName = fileName + '.png';
-        fileData = image.replace(/^data:image\/png;base64,/, '');
-    }
-    else {
-        fileName = fileName + '.jpg';
-        fileData = image.replace(/^data:image\/jpeg;base64,/, '');
-    }
-
-    const filePath = path.join(__dirname, '../public/imgs/' + fileName);
-
-    return { filePath, fileData };
-}
+const Recipe = require('../models/recipe');
 
 router.get('/', (req, res) => {
-    recipe.findAll((error, recipes) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(error || recipes));
-    });
+	Recipe.findAll((error, recipes) => {
+		res.json(error || recipes);
+	});
 });
 
 router.post('/', (req, res) => {
-    const newRecipe = req.body;
-    const { filePath, fileData } = formatImage(newRecipe.name, newRecipe.image);
+	var recipe = Recipe.new(req.body);
 
-    fs.writeFile(filePath, fileData, 'base64', (err) => {
-        if (err) throw err;
-    });
+	Recipe.save(recipe.state, (err, response) => {
+		if(!err) {
+			res.status(201).json({ id: response.insertId });
+		}
+		else {
+			res.status(500).end();
+		}		
+	});
 });
 
 module.exports = router;
--- models/recipe.js
+++ models/recipe.js
@@ -1,29 +1,66 @@
 const dbPool = require('../db/db-connector');
+const path = require('path');
+const fs = require('fs');
 
-const recipe = (recipe) => {
+function saveImage(name, image) {
+	const { filePath, fileData, fileName } = formatImage(name, image);
+	const route = '/imgs/' + fileName;
 
-  let state = recipe;
-
-  return {
-    state
-  } 
+	return new Promise((resolve, reject) => {
+		fs.writeFile(filePath, fileData, 'base64', function (err) {
+			if (err) reject(err);
+			else resolve(route);
+		});
+	});
 }
 
-exports.new = recipe; 
+function formatImage(name, image) {
+	let fileName = name.toLowerCase().replace(' ', '-');
+	let fileData = null;
+
+	if (image.includes('png')) {
+		fileName = fileName + '.png';
+		fileData = image.replace(/^data:image\/png;base64,/, '');
+	}
+	else {
+		fileName = fileName + '.jpg';
+		fileData = image.replace(/^data:image\/jpeg;base64,/, '');
+	}
+
+	const filePath = path.join(__dirname, '../public/imgs/' + fileName);
+
+	return { filePath, fileData, fileName };
+}
+
+const recipe = (recipe) => {
+	let state = recipe;
+
+	return {
+		state
+	}
+}
+
+exports.new = recipe;
 
 exports.findAll = (done) => {
-  dbPool.query('SELECT * FROM Recipes', (err, res, fields) => {
-    if (err) throw err;
+	dbPool.query('SELECT * FROM Recipes', (err, res, fields) => {
+		if (err) throw err;
 
-    let recipes = res.map(r => recipe(r)).map(r => r.state);
-    done(null, recipes);
-  });
+		let recipes = res.map(r => recipe(r)).map(r => r.state);
+		done(null, recipes);
+	});
 };
 
 exports.save = (recipe, done) => {
-  /*dbPool.query('INSERT INTO Recipes SET ?', [recipe], (err, res, fields) => {
-    if (err) throw err;
-
-    done(null, res);
-  });*/
+	saveImage(recipe.name, recipe.image)
+		.then((route) => {
+			recipe.image = route
+			dbPool.query('INSERT INTO Recipes SET ?', [recipe], (err, res) => {
+				if (err) throw err;
+				done(err, res);
+			});
+		})
+		.catch(err => {
+			done(err);
+		});
 }

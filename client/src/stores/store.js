e7cf84a7436c6e07cb21bdb7bbacc2d014093786
Sergio Garcia <srgarcia91@gmail.com>
2017-08-18 21:18:53 +0200

    Basic flux store

---
+++ client/src/stores/store.js
@@ -0,0 +1,29 @@
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  const unsubscribe = (listener) => {
+    listeners = listeners.filter(l => l !== listener);
+  }
+
+  dispatch({});
+  
+  return { getState, dispatch, subscribe, unsubscribe };
+};
+
+export default createStore;

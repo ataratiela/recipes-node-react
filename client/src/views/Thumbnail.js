a43c6a010390521d536fe19985177afad651a0fa
Sergio Garcia <srgarcia91@gmail.com>
2017-08-19 12:14:28 +0200

    Making Thumbnail component more generic

--- client/src/views/Thumbnail.js
+++ client/src/views/Thumbnail.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
 import '../styles/Thumbnail.css';
 
 function Thumbnail(props) {
-  const route = '/recipe/' + props.id;
-
   return (
     <div className='Thumbnail'>
       <img src={ props.image } alt='Recipe'></img>
@@ -14,8 +9,7 @@ function Thumbnail(props) {
         <h3>{ props.title }</h3>
         <p>{ props.description }</p>
         <div className='action-holder'>
-          { /* <a className='btn btn-fill' href='#'>Show</a> */ }
-          <Link className='btn btn-fill' to={ route }>Show</Link>
+          { props.mainLink }
         </div>
       </div>
     </div>

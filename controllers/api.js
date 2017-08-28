22b3d8c0e3e9e27d1bf07d9bde76528106daf9a7
Sergio Garcia <srgarcia91@gmail.com>
2017-08-28 18:24:32 +0200

    API route created and token authentication implemented

---
+++ controllers/api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+router.post('/auth', (req, res) => {
+  const { userId, pass } = req.body;
+
+  User.findById(userID, pass, (err, user) => {
+    if (err) {
+      throw err;
+    } else if (!user || user.passwd !== pass) {
+      res.status(401).json({ success: false, message: 'Invalid user or password' });
+    } else {
+      var token = jwt.sign(user, app.get('secret-token'), {
+        expiresInMinutes: 1440
+      });
+
+      delete user.passwd;
+
+      res.status(200).json({
+        success: true,
+        user: user,
+        token: token
+      });
+    }
+  });
+});
+
+module.exports = router;

84a4c04e51f4f4b3da0c3fbc26ecbee88a05578e
alejalonta@gmail.com <alejalonta@gmail.com>
2017-08-29 23:55:06 +0200

    error component created

---
+++ client/src/views/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Error (props){
+    const errors = props.errors.map((error, i)=>{
+        return <li key={i}>{error}</li>
+    });
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <ul>{ errors }</ul>
+        </div>
+    );
+}
+
+export default Error;

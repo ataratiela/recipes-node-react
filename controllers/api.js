a4eaf45372d38f4f3026215de88d7bb8c77a166b
Sergio Garcia <srgarcia91@gmail.com>
2017-08-30 19:43:29 +0200

    Sending proper errors descriptions on login fails

--- controllers/api.js
+++ controllers/api.js
@@ -13,23 +13,27 @@ router.post('/auth', (req, res) => {
   const { userID, pass } = req.body;
 
   User.findById(userID, (err, user) => {
-    if (err) {
-      throw err;
-    } else if (!user || user.passwd !== pass) {
-      res.status(401).json({ success: false, message: 'Invalid user or password' });
+    if (err) throw err;
+
+    if (user) {
+      if (user.passwd === pass) {
+        const token = jwt.sign(user, req.app.get('secret-token'), {
+          expiresIn: '1d'
+        });
+
+        user.rol = 'client';
+        delete user.passwd;
+
+        res.status(200).json({
+          success: true,
+          user,
+          token
+        });
+      } else {
+        res.status(401).json(['Incorrect password']);
+      }
     } else {
-      const token = jwt.sign(user, req.app.get('secret-token'), {
-        expiresIn: '1d'
-      });
-
-      user.rol = 'client';
-      delete user.passwd;
-
-      res.status(200).json({
-        success: true,
-        user,
-        token
-      });
+      res.status(401).json(['Invalid user']);
     }
   });
 });
@@ -42,18 +46,18 @@ router.use('/users/:user_id', (req, res, next) => {
   const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
   if (token) {
-    jwt.verify(token, req.app.get('secret-token'), (err, decoded) => {      
+    jwt.verify(token, req.app.get('secret-token'), (err, decoded) => {
       if (err) {
-        return res.status(401).json({ success: false, message: 'Failed to authenticate token.' });    
+        return res.status(401).json({ success: false, message: 'Failed to authenticate token.' });
       } else {
-        req.user = decoded;    
+        req.user = decoded;
         next();
       }
     });
   } else {
-    return res.status(403).send({ 
-        success: false, 
-        message: 'No token provided.' 
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
     });
   }
 });

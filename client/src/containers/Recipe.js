89b4eb148514864c11cdae239c52d70d99d311b8
alejalonta@gmail.com <alejalonta@gmail.com>
2017-09-03 17:58:19 +0200

    client side show recipe

--- client/src/containers/Recipe.js
+++ client/src/containers/Recipe.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 
-class Recipes extends Component {
+import axios from 'axios';
+
+import '../styles/Recipes.css';
+
+class Recipe extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      recipes: []
+      recipe: []
     }
   }
 
-  render() {
-    const { id } = this.props.match.params;
+  componentWillMount() {
+    axios.get('/recipes/' + this.props.match.params.id)
+      .then(({ data }) => {
+        this.setState({ recipe: data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
+  render() {
     return (
-      <div>Recipe number { id }</div>
+      <div className='content full-width'>
+        <h3>{this.state.recipe.name}</h3>
+        <img src={this.state.recipe.image} alt='Recipe'></img>
+        <p>{this.state.recipe.description}</p>
+        <p>Number of diners: {this.state.recipe.diners}</p>
+        <p>Preparation time: {this.state.recipe.prepTime} minutes</p>
+      </div>
     );
   }
 }
 
-export default Recipes;
+export default Recipe;

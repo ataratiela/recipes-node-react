2ac8275e4101bd70f66f2d0f4ec5d1d318daad98
Sergio Garcia <srgarcia91@gmail.com>
2017-09-11 21:22:36 +0200

    Refactor inputs for styling purposes

--- client/src/views/RecipeForm.js
+++ client/src/views/RecipeForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from './Header';
+
+const difficultyNames = ['Very Easy', 'Easy', 'Medium', 'Hard', 'Master Chef'];
 
 function RecipeForm(props) {
   const {
@@ -7,138 +10,116 @@ function RecipeForm(props) {
   } = props.recipe;
 
   const recipeImage = image
-    ? <img className='recipe-form-image' src={ 'data:image/png;' + image } alt='Recipe' />
-    : null;
+    ? <img className='recipe-form-image' src={'data:image/png;' + image} alt='Recipe' />
+    : <div className='recipe-image-placeholder'>Select Image...</div>;
 
   const categories = props.categories.map(c => {
-    return <option key={ c.categoryID } value={ c.categoryID }>{ c.name }</option>
+    return <option key={c.categoryID} value={c.categoryID}>{c.name}</option>
+  });
+
+  const difficultyRadios = Array(5).fill(null).map((_, i) => {
+    const index = String(i);
+    return (
+      <label key={i}>
+        <input
+          type='radio'
+          name='difficulty'
+          value={i}
+          checked={difficulty === index}
+          onChange={props.handleInputChange} />
+        {difficultyNames[i]}
+      </label> );
   });
 
   const stepInputs = steps.map((s, i, arr) => {
-    return <div key={ s.id } className='step-input-container'>
-        <input
-          name={ 'step-' + s.id }
-          type='text'
-          placeholder='Step'
-          value={ s.value }
-          onChange={ props.handleStepChange } />
+    return <div key={s.id} className='input-row input--with-addons'>
+      <input
+        name={'step-' + s.id}
+        type='text'
+        placeholder='Step'
+        value={s.value}
+        onChange={props.handleStepChange} />
 
-        { (i === arr.length - 1) 
-            ? <button name={ 'addstep-' + (s.id + 1) } type='button' className='btn btn-secondary' onClick={ props.handleAddState }>+</button>
-            : <button name={ 'rmvstep-' + s.id } type='button' className='btn btn-secondary' onClick={ props.handleRemoveState }>-</button>
-        }
-      </div>
+      {(i === arr.length - 1)
+        ? <button name={'addstep-' + (s.id + 1)} type='button' className='btn btn-addon' onClick={props.handleAddState}>+</button>
+        : <button name={'rmvstep-' + s.id} type='button' className='btn btn-addon' onClick={props.handleRemoveState}>-</button>
+      }
+    </div>
   });
 
-
   return (
-    <form onSubmit={ props.submit } onReset={ props.reset }>
-      <input
-        name='name'
-        type='text'
-        placeholder='Name'
-        value={ name }
-        onChange={ props.handleInputChange } />
-
-      <label>
-        Recipe image
-        <input
-          name='image'
-          type='file'
-          onChange={ props.handleImageChange } />
-        { recipeImage }
-      </label>
-
-      <textarea
-        name='description'
-        placeholder='Recipe description...'
-        rows='5'
-        value={ description }
-        onChange={ props.handleInputChange } />
-
-      <div className='form-select'>
-        <select defaultValue='' value={ category } onChange={ props.handleInputChange }>
-          <option value='' hidden>Category</option>
-          { categories }
-        </select>
+    <form onSubmit={props.submit} onReset={props.reset}>
+    <Header title='Create New Recipe' />
+      <div className='input-row columns'>
+        <div className='recipe-form-image-container'>
+          <label>
+            <input
+              name='image'
+              type='file'
+              onChange={props.handleImageChange} />
+            {recipeImage}
+          </label>
+        </div>
+        <div className='recipe-form-main-description'>
+          <div className='input-row'>
+            <input
+              name='name'
+              type='text'
+              placeholder='Name'
+              value={name}
+              onChange={props.handleInputChange} />
+          </div>
+          <div className='input-row'>
+            <textarea
+              name='description'
+              placeholder='Recipe description...'
+              rows='3'
+              value={description}
+              onChange={props.handleInputChange} />
+          </div>
+          <div className='input-row'>
+            <select defaultValue='' value={category} onChange={props.handleInputChange}>
+              <option value='' hidden>Category</option>
+              {categories}
+            </select>            
+          </div>
+        </div>
       </div>
 
-      <label>
-        Diners:
+      <div className='input-row'>
         <input
           name='diners'
+          placeholder='Diners'
           type='number'
           min='1'
           step='1'
-          value={ diners }
-          onChange={ props.handleInputChange } />
-      </label>
+          value={diners}
+          onChange={props.handleInputChange} />
+      </div>
 
-      <label>
-        Preparation time:
+      <div className='input-row'>
         <input
           name='prepTime'
+          placeholder='Preparation Time'
           type='number'
           min='0'
-          value={ prepTime }
-          onChange={ props.handleInputChange } />
-      </label>
+          value={prepTime}
+          onChange={props.handleInputChange} />
+      </div>
 
-      <span className="rating">
-        Difficulty
-        <input
-          id='difficulty-5'
-          type="radio"
-          name='difficulty'
-          value='5'
-          checked={ difficulty === '5' }
-          onChange={ props.handleInputChange }
-          className="rating-input" />
-        <label htmlFor='difficulty-5' className='rating-star' />
-        <input
-          id='difficulty-4'
-          type="radio"
-          name='difficulty'
-          value='4'
-          checked={ difficulty === '4' }
-          onChange={ props.handleInputChange }
-          className="rating-input" />
-        <label htmlFor='difficulty-4' className='rating-star' />
-        <input
-          id='difficulty-3'
-          type="radio"
-          name='difficulty'
-          value='3'
-          checked={ difficulty === '3' }
-          onChange={ props.handleInputChange }
-          className="rating-input" />
-        <label htmlFor='difficulty-3' className='rating-star' />
-        <input
-          id='difficulty-2'
-          type="radio"
-          name='difficulty'
-          value='2'
-          checked={ difficulty === '2' }
-          onChange={ props.handleInputChange }
-          className="rating-input" />
-        <label htmlFor='difficulty-2' className='rating-star' />
-        <input
-          id='difficulty-1'
-          type="radio"
-          name='difficulty'
-          value='1'
-          checked={ difficulty === '1' }
-          onChange={ props.handleInputChange }
-          className="rating-input" />
-        <label htmlFor='difficulty-1' className='rating-star' />
-      </span>
-      
-      <label>Steps</label>
-      { stepInputs }
+      <div className='input-row'>
+        <span className="rating">
+          Difficulty
+          {difficultyRadios}
+        </span>
+      </div>
 
-      <div className='form-btns'>
+      <Header title='Steps' />
+      {stepInputs}
+
+      <div className='form-actions'>
         <input className='btn btn-fill' type="submit" value="Create" />
-        <input className='btn btn-secondary' type="reset" value="Reset" />
+        <input className='btn' type="reset" value="Reset" />
       </div>
     </form>
   );
